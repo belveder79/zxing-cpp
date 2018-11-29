1c6fdd567595c63c756bc204e89bc2dbfa30c9b2
Clemens Arth <arth@icg.tugraz.at>
2018-11-29 10:19:57 +0100

    update to run process

--- docker/src/server.js
+++ docker/src/server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function ls() {
+  const { stdout, stderr } = await exec('ls');
+  console.log('stdout:', stdout);
+  console.log('stderr:', stderr);
+}
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	ls();
+	res.send('Hello World!');
 });
 
 app.listen(3000, function () {
